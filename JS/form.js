eb7163ac39cd9bdc768c1bca8570b8bd8fb91fab
juanAmprimo <amprimojuangabrielgmail.com>
2025-09-06 11:55:50 -0300

    guardo datos

--- JS/form.js
+++ JS/form.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => { 
 const form = document.getElementById("form");
+const boton = document.querySelector("submit-btn md3-button");
 
-const datos = {
-    nombre: document.getElementById("nombre"),
-    correo: document.getElementById("correo"), 
-    pais: document.getElementById("pais"),
-    tipo_experiencia: getElementById("expe"),
-    Fecha_Nac: document.getElementById("FechaNacimiento"),
-    telefono: document.getElementById("telefono"),
-    apellido: document.getElementById("apellido"),
-};
+boton.addEventListener("click", () => {
+    const datos = {
+      nombre: document.getElementById("nombre").value,
+      correo: document.getElementById("correo").value,
+      pais: document.getElementById("pais").value,
+      tipo_experiencia: document.querySelector("expe").value,
+      Fecha_Nac: document.getElementById("fechaNacimiento").value,
+      telefono: document.getElementById("telefono").value,
+      apellido: document.getElementById("apellido").value,
+    };
 
     localStorage.setItem("usuario", JSON.stringify(datos));
+    alert("Datos guardados correctamente");
+  });
 });
