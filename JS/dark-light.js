4e0d31f3e0a1cfc3c42c900acc0c57000f5577a2
juanAmprimo <amprimojuangabrielgmail.com>
2025-09-06 16:44:25 -0300

    java modo oscuro sincronizado

--- JS/dark-light.js
+++ JS/dark-light.js
@@ -1,9 +1,16 @@
 function cambiarModo() {
-    document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode')) {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('theme', 'dark');
-    } else {
+  } else {
     localStorage.setItem('theme', 'light');
-    }
+  }
 }
+
+window.addEventListener("storage", (e) => {
+  if (e.key === "theme") {
+    const esOscuro = e.newValue === "dark";
+    document.body.classList.toggle("dark-mode", esOscuro);
+  }
+});
 
