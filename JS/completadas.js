a25063733898c8117350b9bb7cb32caf9aa79c3a
Franco Talarico <francotalarico01@gmail.com>
2025-09-07 17:38:41 -0300

    fdsfsd

--- JS/completadas.js
+++ JS/completadas.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const listaCompletadas = document.querySelector('.lista-completadsa');
+    const listaCompletadas = document.querySelector('.lista-completadas');
 
     function obtenerCompletadas() {
         return JSON.parse(localStorage.getItem("tareasCompletadas")) || [];
@@ -9,10 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
         listaCompletadas.innerHTML = "";
         const completadas = obtenerCompletadas();
         completadas.forEach(tarea => {
-            const li = document.createElement("li");
-            li.className = "tarea-completada";
-            li.textContent = tarea;
-            listaCompletadas.appendChild(li);
+        const li = document.createElement("li");
+        li.className = "lista-tareas";
+        li.textContent = tarea;
+        listaCompletadas.appendChild(li);
         });
     }
 
